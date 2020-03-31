1140e122b9ef82248903b148ff639fd1f09d8fb8
Sergey_Kozhevnikov <sergey_kozhevnikov@epam.com>
2020-03-31 15:59:26 +0300

    - update testFindElementWithinParent test - update testNotHealWhenUseInvisibilityOf test - Wrote test for checking bug: https://github.com/healenium/healenium-web/issues/30 Problem with hover - Some tests moved in new class ElementsAndActions

---
+++ src/test/java/com/epam/sha/selenium/ElementsAndActions.java
@@ -0,0 +1,58 @@
+package com.epam.sha.selenium;
+
+import org.junit.Test;
+import pages.AjaxLoadPage;
+import pages.MainPageWithFindBy;
+
+import static org.junit.Assert.assertTrue;
+
+public class ElementsAndActions extends BaseTest {
+
+    @Test
+    public void testWaitForJsElementToDisappear() {
+        AjaxLoadPage mainPage = new AjaxLoadPage(driver);
+        //  Click on `Send Request` button
+        mainPage.open().click();
+        //  Check that spinner is displayed
+        assertTrue("Spinner is not displayed", mainPage.isLoadingSpinnerDisplayed());
+        //  Wait for spinner to disappear
+        mainPage.waitForLoadingSpinnerToDisappear();
+        //  Check that spinner is not displayed
+        assertTrue("Content loaded is not displayed", mainPage.isLoadingContentDisplayed());
+    }
+
+    @Test
+    public void testHover() {
+        /*
+        Checking a bug: https://github.com/healenium/healenium-web/issues/30
+         */
+        MainPageWithFindBy mainPage = new MainPageWithFindBy(driver).open();
+        mainPage.hoverOnTestButton();
+        String class_of_element = mainPage.getAttributeFromTestButton("class");
+        assertTrue("In the class not indicated that on the button hovered a cursor",
+                class_of_element.contains("button_hovered"));
+    }
+
+    @Test
+    public void testNotHealWhenUseInvisibilityOf() {
+        /*
+        Checking a bug: https://github.com/healenium/healenium-web/issues/31
+         */
+        MainPageWithFindBy mainPage = new MainPageWithFindBy(driver).open();
+        mainPage.clickButtonForInvisible();
+        assertTrue("The error while checking the invisibility of the element",
+                mainPage.checkThatButtonInvisible());
+    }
+
+    @Test
+    public void testFindElementWithinParent() {
+        /*
+        Checking the bug: https://github.com/healenium/healenium-web/issues/43
+         */
+        MainPageWithFindBy mainPage = new MainPageWithFindBy(driver).open();
+        String text = mainPage.takeValueFromSecondButton();
+        assertTrue("The text doesn't match with the expected result",
+                text.equalsIgnoreCase("Second"));
+    }
+
+}
--- src/test/java/com/epam/sha/selenium/MarkupTest.java
+++ src/test/java/com/epam/sha/selenium/MarkupTest.java
@@ -1,11 +1,8 @@
 package com.epam.sha.selenium;
 
 import org.junit.Test;
-import pages.AjaxLoadPage;
 import pages.MainPage;
 import pages.MainPageWithFindBy;
-import pages.MainPageYandex;
-import static org.junit.Assert.assertTrue;
 
 
 public class MarkupTest extends BaseTest {
@@ -38,42 +35,4 @@ public class MarkupTest extends BaseTest {
             mainPage.confirmAlert();
         }
     }
-
-    @Test
-    public void testWaitForJsElementToDisappear() {
-        AjaxLoadPage mainPage = new AjaxLoadPage(driver);
-        //  Click on `Send Request` button
-        mainPage.open().click();
-        //  Check that spinner is displayed
-        assertTrue("Spinner is not displayed", mainPage.isLoadingSpinnerDisplayed());
-        //  Wait for spinner to disappear
-        mainPage.waitForLoadingSpinnerToDisappear();
-        //  Check that spinner is not displayed
-        assertTrue("Content loaded is not displayed", mainPage.isLoadingContentDisplayed());
-    }
-
-    @Test
-    public void testFindElementWithinParent() {
-        /*
-        Checking the bug: https://github.com/healenium/healenium-web/issues/43
-         */
-        MainPageYandex mainYandex = new MainPageYandex(driver).open();
-        String text = mainYandex.takeTextFromButton();
-        assertTrue("Text doesn't match with expected result", text.equalsIgnoreCase("найти"));
-    }
-
-    @Test
-    public void testNotHealWhenUseInvisibilityOf() {
-        /*
-        Checking a bug: https://github.com/healenium/healenium-web/issues/31
-         */
-        MainPageWithFindBy mainPage = new MainPageWithFindBy(driver).open();
-        mainPage
-                .clickTestButton()
-                .confirmAlert();
-        new MainPageYandex(driver).open();
-        assertTrue("The error while checking element invisible",
-                mainPage.checkThatButtonInvisible());
-    }
-
 }
